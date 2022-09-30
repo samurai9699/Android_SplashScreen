8756ffe6f338a2ec77d252b068b364287ee61cf8
samurai9699 <samuelmuriithi965@gmail.com>
2022-09-30 14:59:38 +0300

    done

---
+++ app/src/main/java/com/example/android_splashscreen/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.android_splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val backgroundImage: ImageView = findViewById(R.id.SplashScreenImage)
+        val backgroundText: TextView = findViewById(R.id.textView)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
+        backgroundImage.startAnimation(slideAnimation)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
